f08b8ec070098287c92870422f78d5a490546de9
Max Palomino <maxpalominomendoza@gmail.com>
2021-11-15 21:49:26 -0500

    swagger doc

---
+++ README.md
@@ -0,0 +1,12 @@
+# Getting Started with Api
+### `npm i`
+### `npm run dev`
+```
+Example app listening at port http://localhost:5000
+[nodemon] restarting due to changes...
+[nodemon] starting `node server.js`
+```
+### `Swagger Documentation`
+```
+GET http://localhost:5000/api-docs/
+```
--- index.js
+++ index.js
@@ -1,27 +1,34 @@
-require('dotenv').config();
-const errorMiddleware = require('./src/middlewares/error.middleware');
+const swaggerUi = require('swagger-ui-express')
+const YAML = require('yamljs')
+require('dotenv').config()
+const errorMiddleware = require('./src/middlewares/error.middleware')
 
 const express = require('express')
-const parser = require('body-parser');
+const parser = require('body-parser')
 const app = express()
 
 // application/x-www-form-urlencoded
-app.use(parser.urlencoded({ extended: false }));
+app.use(parser.urlencoded({ extended: false }))
 // application/json
-app.use(parser.json());
+app.use(parser.json())
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
-  next();
-});
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method'
+  )
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE')
+  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE')
+  next()
+})
 
+const swaggerDocument = YAML.load('./swagger.yaml')
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
-require('./src/routes/api')(app);
+require('./src/routes/api')(app)
 
 // Error Handler Middleware
-app.use(errorMiddleware);
+app.use(errorMiddleware)
 
 module.exports = app
